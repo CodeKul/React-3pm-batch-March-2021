26ffc67ff71735b2dac9dc9b159d0d0da499bca4
sanketcodekul <sanketbhosale.codekul.com>
2021-05-19 16:07:53 +0530

    React Redux example

---
+++ redux-ex-2/src/reducers/postReducer.js
@@ -0,0 +1,29 @@
+import {useState} from "react";
+
+// export const [intialState, setInitialState] = useState(
+//     {
+
+//         posts: [],
+//         loading: false,
+//         hasErrors: false
+//     }    
+// )
+
+
+export const initialState =
+    {
+
+        posts: [],
+        loading: false,
+        hasErrors: false
+    }    
+
+
+
+export default function postReducer(state = initialState, action) {
+    
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
